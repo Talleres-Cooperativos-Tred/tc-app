5b1477aab4927ee54d80e01ba9820d85b2e0f060
Tomyred <tomy.rojo.1@gmail.com>
2022-04-01 19:35:42 -0300

    login

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
+import { useState } from "react";
 import { Provider } from "react-redux";
 import "./App.css";
 import Login from "./components/login/login";
 import Movies from "./components/movies/movies";
 import generateStore from "./store";
 
+const users = [
+    {
+        userName: "Tred",
+        password: "tredinstructor12",
+    },
+    {
+        userName: "adrianDev",
+        password: "adrianDevpass12",
+    },
+    {
+        userName: "lu15",
+        password: "lupass15",
+    },
+];
+
 function App() {
+    const [user, setUser] = useState({ userName: "", password: "" });
+    const [loginSucceed, setLoginSucceed] = useState(false);
+    const [loginError, setLoginError] = useState(false);
+    const [instance, setInstance] = useState("login");
     const store = generateStore();
-    const users = [
-        {
-            userName: "Tred",
-            password: "tredinstructor12",
-        },
-        {
-            userName: "adrianDev",
-            password: "adrianDevpass12",
-        },
-        {
-            userName: "lu15",
-            password: "lupass15",
-        },
-    ];
+
+    const handleLogin = () => {
+        if (instance === "login") {
+            const success = users.find(element => {
+                return (
+                    element.userName === user.userName &&
+                    element.password === user.password
+                );
+            });
+
+            if (!success) {
+                setLoginSucceed(false);
+                setLoginError(true);
+            } else {
+                setLoginSucceed(true);
+            }
+        } else {
+            users.push(user);
+            setInstance("login");
+        }
+    };
+
     return (
         <Provider store={store}>
-            <Login />
+            {loginSucceed === false ? (
+                <Login
+                    setUser={setUser}
+                    user={user}
+                    handleLogin={handleLogin}
+                    loginError={loginError}
+                    setInstance={setInstance}
+                    instance={instance}
+                />
+            ) : (
+                <Movies />
+            )}
         </Provider>
     );
 }
--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,10 +1,56 @@
 import React from "react";
+import "./login.css";
 
-const Login = () => {
+const Login = ({
+    setUser,
+    user,
+    handleLogin,
+    loginError,
+    instance,
+    setInstance,
+}) => {
+    const handleChange = e => {
+        const newUser = { ...user, [e.target.name]: e.target.value };
+        setUser(newUser);
+    };
+
     return (
-        <div>
-            <input type="text" />
-            <input type="text" />
+        <div className="login">
+            <h2> {instance === "login" ? "Iniciar Sesion" : "Registrate"} </h2>
+            {loginError === false ? (
+                ""
+            ) : (
+                <span style={{ color: "red" }}>
+                    El usuario o la contraseña son incorrectos
+                </span>
+            )}
+            <div>
+                <input
+                    name="userName"
+                    onChange={handleChange}
+                    placeholder="Usuario"
+                    type="text"
+                />
+                <input
+                    name="password"
+                    onChange={handleChange}
+                    placeholder="Contraseña"
+                    type="text"
+                />
+                <button onClick={handleLogin}>Ingresar</button>
+            </div>
+            {instance === "login" ? (
+                <span
+                    onClick={() => setInstance("dsadsa")}
+                    className="register"
+                >
+                    Registrarse
+                </span>
+            ) : (
+                <span onClick={() => setInstance("login")} className="register">
+                    Iniciar sesion
+                </span>
+            )}
         </div>
     );
 };
