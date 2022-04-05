32ba2be4db35181861a5a2036fb1dd2e590a2b15
Tomyred <tomy.rojo.1@gmail.com>
2022-04-05 11:11:44 -0300

    radius input

--- src/app/main/avatarGen/avatarGen.js
+++ src/app/main/avatarGen/avatarGen.js
@@ -6,7 +6,8 @@ const AvatarGen = () => {
     const [text, setText] = useState("tomy");
     const [avatarType, setAvatarType] = useState(1);
 
-    const handleClick = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
         setText(input);
         if (input.length === 0) {
             setText("tomy");
@@ -16,22 +17,25 @@ const AvatarGen = () => {
     return (
         <div className="avatar__container">
             <h1>Create your Avatar!</h1>
-            <input
-                type="text"
-                value={input}
-                placeholder="Type something..."
-                onChange={e => setInput(e.target.value)}
-            />
-            <button onClick={handleClick}>Give my avatar</button>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={input}
+                    placeholder="Type something..."
+                    onChange={e => setInput(e.target.value)}
+                />
+                <button type="submit">Give my avatar</button>
+            </form>
+
             <img
                 src={`https://robohash.org/${text}?set=set${avatarType}`}
                 alt={text}
             />
             <div className="radio__container">
-                <input onClick={() => setAvatarType(1)} type="radio" />
-                <input type="radio" />
-                <input type="radio" />
-                <input type="radio" />
+                <input type="radio" value="1" defaultChecked name="name" />
+                <input name="name" value="2" type="radio" />
+                <input name="name" value="3" type="radio" />
+                <input name="name" value="4" type="radio" />
             </div>
         </div>
     );
